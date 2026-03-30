05797e02c0e7f6b56a1480d278aebae8b0056098
modotz <modotzprakoso@gmail.com>
2026-03-30 18:59:04 +0700

    change google maps

--- app.js
+++ app.js
@@ -377,8 +377,8 @@ io.on("connection", (socket) => {
     const transport = await router.createWebRtcTransport({
       listenIps: [{
         ip: "0.0.0.0",
-        //announcedIp: "192.167.61.17" // IP server local
-        announcedIp: "192.168.100.5" // IP Ralika
+        announcedIp: "192.167.61.17" // IP server local
+        //announcedIp: "192.168.100.5" // IP Ralika
         //announcedIp: "192.168.10.201" // IP server
       }],
       initialAvailableOutgoingBitrate: 1000000,
